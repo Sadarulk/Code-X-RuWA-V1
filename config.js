85f689d44fc41fb023ef24658fb3b4008fbd1644
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-19 19:20:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "n252RBYI#vA0w58pwTZbr2Du9baHD6ALX-Ldk79R03OxrD3UyttY",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Sadarulk/QueenMatheeDB/blob/main/botlogos/botlogo.jpg?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "𝐇𝐞𝐥𝐥𝐨, 𝐈 𝐀𝐦 𝘾𝙤𝙙𝙚-𝙓-𝙍𝙪𝙬𝙖-𝙑1[ ᵐᵈ ʷʰᵃᵗˢᵃᵖᵖ ᵇᵒᵗ ] 𝐈 𝐀𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰!Oᴡɴᴇʀ ɴᴀᴍᴇ - 𝗜𝘀𝗵𝗮𝗿𝗮 𝗥𝘂𝘄𝗮𝗻 Oᴡɴᴇʀ WA Nᴜᴍʙ - +94725337377Gɪᴛʜᴜʙ Rᴇᴘᴏ - 𝒄𝒐𝒎𝒊𝒏𝒈 𝒔𝒐𝒐𝒏…Mᴀɪɴ Bᴏᴛ WA Gʀᴏᴜᴘ -https://chat.whatsapp.com/CViytr0TGpM4gLedmxnFi5",
+SESSION_ID: process.env.SESSION_ID || "Enter your session id here",
 };

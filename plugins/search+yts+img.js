0dd06a3491a05a88fa5f0a32ed29942a98d07e20
💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️ <121302725+DUKEKOLLA@users.noreply.github.com>
2024-11-29 20:50:01 +0530

    💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️

--- plugins/search+yts+img.js
+++ plugins/search+yts+img.js
@@ -154,7 +154,7 @@ cmd(
             caption:
               '\n*\uD83D\uDC97Image ' +
               (_0x4be3c5 + 1) +
-              ' from your search!\uD83D\uDC97*\n\n *Enjoy these images! \uD83D\uDC7E*\n\n>ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1',
+              ' from your search!\uD83D\uDC97*\n\n *Enjoy these images! \uD83D\uDC7E*\n\n> ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1',
           },
           { quoted: _0x40dd7d }
         )

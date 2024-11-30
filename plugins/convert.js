3d63023b9e1f5493fa15e8cea3af56de93fdaff1
💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️ <121302725+DUKEKOLLA@users.noreply.github.com>
2024-12-01 03:46:12 +0530

    💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️

--- plugins/convert.js
+++ plugins/convert.js
@@ -3,7 +3,7 @@ const { cmd, commands } = require('../command');
 const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter');
 const googleTTS = require('google-tts-api');
 const { getRandom } = require('../lib/functions');
-const ffmpeg = require('fluent-ffmpeg');
+
 const imgMsg = 'Reply to a photo for sticker!';
 const description = 'It converts your replied photo to a sticker.';
 
@@ -140,39 +140,4 @@ cmd({
 
 
 
- cmd({ 
-    pattern: '.url',
-    fromMe: isPublic,
-    desc: "upload files to imgur.com",
-    type: "owner",
-  },
-  async (message, match) => {
-    if (message.reply_message.audio) {
-      try {
-        var media = await message.reply_message.download();
-        ffmpeg(media).outputOptions(["-y", "-filter_complex", "[0:a]showvolume=f=1:b=4:w=720:h=68,format=yuv420p[vid]", "-map", "[vid]", "-map 0:a"]).save('output.mp4').on('end', async () => {
-        var res = await upload('output.mp4');
-        await message.reply(res.link);
-        });
-      } catch (e) {
-	await message.reply(e.message)
-      }
-    }
-
-    else if (message.reply_message.image || message.reply_message.video) {
-      try {
-        var media = await message.reply_message.download();
-        var res = await upload(media);
-        await message.reply(res.link)
-      } catch (e) {
-	await message.reply(e.message)
-      }
-    }
-
- else return await message.reply("_Reply to image/video/audio_");
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-  
+ 

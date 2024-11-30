e7bc8a5d8a864e137a3fb2396d806c3002c74fea
💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️ <121302725+DUKEKOLLA@users.noreply.github.com>
2024-11-30 20:24:57 +0530

    💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️

---
+++ plugins/ishara (1).js
@@ -0,0 +1,47 @@
+const {cmd,commands} = require("../command");
+
+cmd({
+  'on': "body"
+
+
+}, async (_0x20df9f, _0x1f42a4, _0x156caf, {
+  from: _0x1879a0,
+  quoted: _0x41bc42,
+  body: _0xbb0b45,
+  isCmd: _0xaafba2,
+  command: _0x2ea859,
+  args: _0x29f92f,
+  q: _0xa78096,
+  isGroup: _0x2fd8b3,
+  sender: _0x12075c,
+  senderNumber: _0x518b22,
+  botNumber2: _0x55f0f4,
+  botNumber: _0xec8c86,
+  pushname: _0xc489ac,
+  isMe: _0x3ac0a2,
+  isOwner: _0x4830fd,
+  groupMetadata: _0x3efd1d,
+  groupName: _0x463478,
+  participants: _0x837540,
+  groupAdmins: _0x5599f8,
+  isBotAdmins: _0x1508cc,
+  isAdmins: _0x431b21,
+  reply: _0x1b773d
+
+}) => {
+  try {
+    if (_0x518b22 === _0xec8c86) {
+      return;
+    }
+    if (_0xbb0b45 === 'Ishara') {
+      _0x1b773d("----------\n\n*Ishara Ruwan is CodeXruwa Bot Owner.*\n\n*🌟 A short about of Ishara ⤵*\n\n*Name :* Ishara Ruwan\n*Age :* 27+ 🎉\n*Lives in :* Daraniyagala 🌍\n\n*Skills:*\n- JavaScript Developer 💻\n- Problem Solver 🧩\n- Creative Thinker 💡\n\n*Hobbies:*\n- Bot Developing 🤖\n- Learning Programming 📚\n- Learning for Life 🌱\n\n----------\n\n> ᴄᴏᴅᴇXʀᴜᴡᴀ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ɪsʜᴀʀᴀ");
+    } else if (_0xbb0b45 === 'ishara') {
+      _0x1b773d("----------\n\n*Ishara Ruwan is CodeXruwa Bot Owner.*\n\n*🌟 A short about of Ishara ⤵*\n\n*Name :* Ishara Ruwan\n*Age :* 27+ 🎉\n*Lives in :* Daraniyagala 🌍\n\n*Skills:*\n- JavaScript Developer 💻\n- Problem Solver 🧩\n- Creative Thinker 💡\n\n*Hobbies:*\n- Bot Developing 🤖\n- Learning Programming 📚\n- Learning for Life 🌱\n\n----------\n\n> ᴄᴏᴅᴇXʀᴜᴡᴀ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ɪsʜᴀʀᴀ");
+    }
+ 
+
+ } catch (_0x303b7f) {
+    console.log(_0x303b7f);
+    _0x1b773d('' + _0x303b7f);
+  }
+});

e15d5bb5c0c7f9617fc6e79de8870fa398fa2b52
💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️ <121302725+DUKEKOLLA@users.noreply.github.com>
2024-12-02 10:16:59 +0530

    💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "mongodb://mongo:yqzUFafwPiAfqersdkRLcGAeDdYzbvuw@junction.proxy.rlwy.net:59749",
+SESSION_ID: process.env.SESSION_ID || "", //put you session id
+MONGODB: process.env.MONGODB || "mongodb://mongo:yqzUFafwPiAfqersdkRLcGAeDdYzbvuw@junction.proxy.rlwy.net:59749", //dont change
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",

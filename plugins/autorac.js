b9e234ed2ca75c8df1b28ce4ca4c113af9bbf452
💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️ <121302725+DUKEKOLLA@users.noreply.github.com>
2024-12-02 10:03:03 +0530

    💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️

---
+++ plugins/autorac.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {       
+ if (config.FAKE_RECORDING === 'true') {
+                await conn.sendPresenceUpdate('recording', from);
+            }
+         } 
+   );

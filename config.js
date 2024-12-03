043117ec300a712f37e0415f87a7031d6aa13395
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-12-03 09:58:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put you session id
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Wj4FxKYA#u5LHIfoP0L3SccXOxZTTg54MFLSvZ1agd9LlKj2mR1M", //put you session id
 MONGODB: process.env.MONGODB || "mongodb://mongo:jGfcdMGNcfyTPJClutUKSrBBBfbpPmez@junction.proxy.rlwy.net:24134", //dont change
-READ_MESSAGE: process.env.READ_MESSAGE || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",    
 };
 
